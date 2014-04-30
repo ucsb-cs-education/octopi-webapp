7707884cfa9df7bd2ed7744ec48910308865d499
Alex Simes <alex.simes29@gmail.com>
2014-04-30 10:53:30 -0700

    add student to class bug fix, drop down for student login, show students currently in class

--- app/models/student.rb
+++ app/models/student.rb
@@ -15,13 +15,13 @@ class Student < ActiveRecord::Base
     SecureRandom.urlsafe_base64
   end
 
-  def Student.hash(token)
+  def Student.create_remember_hash(token)
     Digest::SHA1.hexdigest(token.to_s)
   end
 
   private
     def create_remember_token
-      self.remember_token = Student.hash(Student.new_remember_token)
+      self.remember_token = Student.create_remember_hash(Student.new_remember_token)
     end
 
 end
