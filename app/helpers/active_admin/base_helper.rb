e7f899b379ed9c25061ba438c6f8ae1d084537d1
Johan Henkens <Johan@henkens.com>
2014-08-06 15:04:48 -0700

    Fix activeadmin helper

--- app/helpers/active_admin/base_helper.rb
+++ app/helpers/active_admin/base_helper.rb
@@ -1,6 +1,6 @@
-module ActiveAdmin::ViewsHelper
+module ActiveAdmin::BaseHelper
   def devise_mapping
-    @devise_mapping ||= request.env["devise.mapping"]
+    @devise_mapping ||= request.env['devise.mapping']
     @devise_mapping ||= Devise.mappings[:staff]
   end
 
