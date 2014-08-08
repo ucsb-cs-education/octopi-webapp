b3229abdcddcf9088471fa822d5c5a41903d99a5
Johan Henkens <Johan@henkens.com>
2014-08-08 12:08:34 -0700

    add force processing to laplaya analysis

--- app/models/laplaya_file/student_response/task_response_laplaya_file.rb
+++ app/models/laplaya_file/student_response/task_response_laplaya_file.rb
@@ -2,8 +2,8 @@ class StudentResponse::TaskResponseLaplayaFile < StudentResponse::StudentRespons
   belongs_to :laplaya_task_response, foreign_key: :parent_id
   after_update :trigger_analysis
 
-  def trigger_analysis
-    if project_changed?
+  def trigger_analysis(force = false)
+    if project_changed? || force
       Resque.enqueue(ProcessLaplayaFileById,
                      id, laplaya_task_response.task.laplaya_analysis_file.id, parent_id)
     end
