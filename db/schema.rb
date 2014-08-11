f3cb0a86e4a5b3d54eec336a547726c82096b45c
Johan Henkens <Johan@henkens.com>
2014-08-11 10:26:47 -0700

    Fix flashes for unsupported browsers on homepage

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140809041417) do
+ActiveRecord::Schema.define(version: 20140810174925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
