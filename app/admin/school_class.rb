3d3a077ee056477dea25bb4f487b3cb78786c33d
Jahwffrey <johnthomason@umail.ucsb.edu>
2014-08-13 16:41:16 -0700

    Various improvements to activeadmin panel

--- app/admin/school_class.rb
+++ app/admin/school_class.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register SchoolClass do
-  actions :all, except: [:edit, :new]
+  actions :all, except: [:new]
   permit_params :name, :ip_range, :student_remote_access_allowed, students: []
   filter :school, :collection => proc { School.accessible_by(current_ability, :read) }
 
@@ -12,7 +12,13 @@ ActiveAdmin.register SchoolClass do
   end
 
   show :title => :name do
-    attributes_table :name, :school do
+    attributes_table do
+      [:name, :school].each do |attribute|
+        row attribute
+      end
+      row 'Edit' do
+        link_to 'Edit Class', edit_school_class_path(school_class)
+      end
       table_for school_class.students.where(type: "Student").order('first_name ASC') do
         column 'Students' do |student|
           link_to student.name, [:admin, student]
@@ -26,6 +32,12 @@ ActiveAdmin.register SchoolClass do
       f.input :name
     end
     f.actions
+  end
+
+  controller do
+    def edit
+      redirect_to edit_school_class_path(SchoolClass.find(params['id']))
+    end
   end
 
 end
