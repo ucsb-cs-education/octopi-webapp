43c9fadd3c919336af7166eadeb4d7aefb83f2e0
Jahwffrey <johnthomason@umail.ucsb.edu>
2014-08-14 09:46:26 -0700

    Bugfix

--- app/admin/test_student.rb
+++ app/admin/test_student.rb
@@ -6,13 +6,19 @@ ActiveAdmin.register TestStudent do
   filter :school, :collection => proc { School.accessible_by(current_ability, :read) }
   filter :school_classes, :collection => proc { SchoolClass.where(school: School.accessible_by(current_ability, :read)) }
 
-
   index do
     if current_staff.super_staff?
       selectable_column
       id_column
       column :staff
       actions
+    end
+  end
+
+  controller do
+    def show
+      staff = TestStudent.find(params[:id]).staff
+      redirect_to admin_staff_path(staff)
     end
   end
 end
