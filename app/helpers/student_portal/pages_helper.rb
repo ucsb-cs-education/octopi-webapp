f7e1536eb19caf06275425790f4dc7ac293ac7d6
Johan Henkens <Johan@henkens.com>
2014-08-18 12:07:08 -0700

    Revert Cross origin

--- app/helpers/student_portal/pages_helper.rb
+++ app/helpers/student_portal/pages_helper.rb
@@ -1,5 +1,9 @@
 module StudentPortal::PagesHelper
 
+  def current_class_modules_for_select
+    current_school_class.module_pages.order(parent_id: :asc, position: asc)
+  end
+
   def get_path_for_task(task)
     if task
       case task.type
