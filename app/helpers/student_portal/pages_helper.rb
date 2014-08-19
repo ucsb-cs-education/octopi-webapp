d12f5ee7082f9a80b9a2ba31bc98e0f3f8e4516f
Johan Henkens <Johan@henkens.com>
2014-08-18 18:27:15 -0700

    Sort student portal module select

--- app/helpers/student_portal/pages_helper.rb
+++ app/helpers/student_portal/pages_helper.rb
@@ -1,7 +1,7 @@
 module StudentPortal::PagesHelper
 
   def current_class_modules_for_select
-    current_school_class.module_pages.order(parent_id: :asc, position: asc)
+    current_school_class.module_pages.order(page_id: :asc, position: :asc)
   end
 
   def get_path_for_task(task)
