e92b1b214843d1dbaf5b45754b02c30577110554
Jahwffrey <johnthomason@umail.ucsb.edu>
2014-08-20 09:56:42 -0700

    Fixed the path for reseting task responses for teachers

--- spec/controllers/task_responses_controller_spec.rb
+++ spec/controllers/task_responses_controller_spec.rb
@@ -29,25 +29,25 @@ describe TaskResponsesController, type: :controller do
       it 'should remove the response' do
         #it 'should not remove the response' do
         expect do
-          xhr :delete, :reset, id: laplaya_response, task_response_id: school_class
+          xhr :delete, :reset, id: laplaya_response, school_class_id: school_class
         end.to change(LaplayaTaskResponse, :count).by (-1)
         #end.to_not change(LaplayaTaskResponse, :count)
       end
       it 'should delete a laplaya file' do
         expect do
-          xhr :delete, :reset, id: laplaya_response, task_response_id: school_class
+          xhr :delete, :reset, id: laplaya_response, school_class_id: school_class
         end.to change(LaplayaFile, :count).by (-1)
       end
       it 'should not change the number of unlocks' do
         #it 'should not lock any task responses' do
         expect do
-          xhr :delete, :reset, id: laplaya_response, task_response_id: school_class
+          xhr :delete, :reset, id: laplaya_response, school_class_id: school_class
         end.to_not change(Unlock, :count)
         #end.to_not change(TaskResponse.unlocked, :count)
       end
       it 'should make a task response be no longer completed' do
         expect do
-          xhr :delete, :reset, id: laplaya_response, task_response_id: school_class
+          xhr :delete, :reset, id: laplaya_response, school_class_id: school_class
         end.to change(TaskResponse.completed, :count).by (-1)
       end
     end
@@ -62,32 +62,32 @@ describe TaskResponsesController, type: :controller do
       it 'should remove the response' do
         #it 'should not remove the response' do
         expect do
-          xhr :delete, :reset, id: assessment_response, task_response_id: school_class
+          xhr :delete, :reset, id: assessment_response, school_class_id: school_class
         end.to change(AssessmentTaskResponse, :count).by (-1)
         #end.to_not change(AssessmentTaskResponse, :count)
       end
       it 'should delete an assessment question response' do
         expect do
-          xhr :delete, :reset, id: assessment_response, task_response_id: school_class
+          xhr :delete, :reset, id: assessment_response, school_class_id: school_class
         end.to change(AssessmentQuestionResponse, :count).by (-1)
       end
       it 'should not change the number of unlocks' do
         #it 'should not lock any task responses' do
         expect do
-          xhr :delete, :reset, id: assessment_response, task_response_id: school_class
+          xhr :delete, :reset, id: assessment_response, school_class_id: school_class
         end.to_not change(Unlock, :count)
         #end.to_not change(TaskResponse.unlocked, :count)
       end
       it 'should make a task response be no longer completed' do
         expect do
-          xhr :delete, :reset, id: assessment_response, task_response_id: school_class
+          xhr :delete, :reset, id: assessment_response, school_class_id: school_class
         end.to change(TaskResponse.completed, :count).by (-1)
       end
       it 'should make an unlock be no longer hidden' do
-      #it 'should make a task response be no longer hidden' do
+        #it 'should make a task response be no longer hidden' do
         expect do
-          xhr :delete, :reset, id: assessment_response, task_response_id: school_class
-          end.to change(Unlock.where(hidden: true), :count).by(-1)
+          xhr :delete, :reset, id: assessment_response, school_class_id: school_class
+        end.to change(Unlock.where(hidden: true), :count).by(-1)
         #end.to change(TaskResponse.where(hidden: true), :count).by (-1)
       end
     end
