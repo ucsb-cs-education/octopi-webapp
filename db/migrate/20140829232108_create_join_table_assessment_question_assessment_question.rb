62e454a9b4be64bf350cf0b2c5c07142ce97a39c
Jahwffrey <johnthomason@umail.ucsb.edu>
2014-09-02 14:37:55 -0700

    Student view of an assessment question now randomly selects one of the variants.

--- db/migrate/20140829232108_create_join_table_assessment_question_assessment_question.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableAssessmentQuestionAssessmentQuestion < ActiveRecord::Migration
-  def change
-    create_table "question_relation", :force => true, :id => false do |t|
-      t.integer "question_a_id", :null => false
-      t.integer "question_b_id", :null => false
-    end
-  end
-end
