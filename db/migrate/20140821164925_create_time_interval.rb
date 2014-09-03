ccd535d59aeb32ec2161f35d3ff0c1568e4f59d9
Jahwffrey <johnthomason@umail.ucsb.edu>
2014-09-03 09:21:14 -0700

    In progress

--- db/migrate/20140821164925_create_time_interval.rb
+++ db/migrate/20140821164925_create_time_interval.rb
@@ -1,5 +1,6 @@
 class CreateTimeInterval < ActiveRecord::Migration
   def change
+    add_column :task_responses, :time_intervals, :text, default: "[]"
     create_table :time_intervals do |t|
       t.integer :begin_time
       t.integer :end_time
