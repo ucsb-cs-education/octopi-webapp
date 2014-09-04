a8015ac0824478374b97e9bb9d2e810b5e891f7f
Jahwffrey <johnthomason@umail.ucsb.edu>
2014-09-04 16:40:18 -0700

    Fixed assessment tasks with variants being unsaveable and other bugs

--- app/controllers/task_responses_controller.rb
+++ app/controllers/task_responses_controller.rb
@@ -1,5 +1,7 @@
 class TaskResponsesController < ApplicationController
   load_and_authorize_resource :task_response
+  before_action :verify_assessment_task
+  #Im going to scope only being able to see ones that provide feedback into ability.rb
 
   def show
     @student = Student.find(@task_response.student)
@@ -8,5 +10,12 @@ class TaskResponsesController < ApplicationController
     @activity_page = @assessment_task.activity_page
     @module_page = @activity_page.module_page
     @assessment_question_responses = @task_response.assessment_question_responses
+  end
+
+  def verify_assessment_task
+    unless @task_response.task.is_a?(AssessmentTask)
+      flash[:warning] = "You cannot view that page."
+      redirect_to root_path
+    end
   end
 end
