b284c3b9d73319751f0db9937ae7914a0887ed6f
Jahwffrey <johnthomason@umail.ucsb.edu>
2014-09-05 15:29:54 -0700

    Fixed pemissions, improved navigation, etc

---
+++ app/assets/javascripts/task_responses.js.coffee
@@ -0,0 +1,5 @@
+TaskResponsesController = Paloma.controller('TaskResponses');
+
+TaskResponsesController.prototype.show = () ->
+  $("#change-student").change ->
+    $(this).attr "action", $("#change-student option:selected").val()
--- app/controllers/task_responses_controller.rb
+++ app/controllers/task_responses_controller.rb
@@ -10,20 +10,36 @@ class TaskResponsesController < ApplicationController
     @activity_page = @assessment_task.activity_page
     @module_page = @activity_page.module_page
     @assessment_question_responses = @task_response.assessment_question_responses
+
+    #for the dropdown
+    @students = @school_class.students.not_teststudents.includes(:task_responses).where(:task_responses => {:task_id => @assessment_task, :school_class_id => @school_class}).order('last_name')
+    if current_student
+      @students << current_student if current_student.task_responses.find_by(task: @assessment_task, school_class: @school_class)
+    end
+    @responses_map = @students.map { |s|
+      s.task_responses.find_by(task: @assessment_task, school_class: @school_class).id
+    }
   end
 
-  def index
+def index
     if params['task'].present?
       authorize! :read, @task = Task.find(params['task'])
-      @school_classes = SchoolClass.accessible_by(@current_ability)
+      @school_classes = SchoolClass.accessible_by(@current_ability).order('name')
       @task_responses = @task_responses.where(task: @task)
       @response_map = @school_classes.map { |sc|
-        {:name => sc.name, :responses => @task_responses.where(school_class: sc).map {|tr|
-          {:student_name => tr.student.name,:response_id => tr.id }
-        }}
+        #show only my students and my test student
+        students = sc.students.not_teststudents << current_student
+        {:name => sc.name, :responses => @task_responses.where(school_class: sc, student: students).map { |tr|
+          {:student_first_name => tr.student.first_name,
+           :student_last_name => tr.student.last_name,
+           :response_id => tr.id}
+        }.sort_by! { |hsh| hsh[:student_last_name] }}
       }
     else
-      #list them all? Could be ALOT of data.
+      #Here it might instead be a good idea to create a page to
+      #view all of the task responses visible to you?
+      flash[:warning] = "You must specify a task to view the responses of."
+      redirect_to root_path
     end
   end
 
@@ -45,9 +61,20 @@ class TaskResponsesController < ApplicationController
     end
   end
 
+  private
   def verify_assessment_task
-    unless @task_response.task.is_a?(AssessmentTask)
-      flash[:warning] = "You cannot view that page."
+    if params['task'].present?
+      authorize! :read, @task = Task.find(params['task'])
+      if @task.type != 'AssessmentTask'
+        flash[:error] = "Only responses to question tasks can be viewed there."
+        redirect_to root_path
+      end
+    end
+  end
+
+  def verify_assessment_task_response
+    unless @task_response.is_a?(AssessmentTaskResponse)
+      flash[:error] = "Only responses to question tasks can be viewed there."
       redirect_to root_path
     end
   end
