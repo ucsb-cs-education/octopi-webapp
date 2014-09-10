d81e3d24c3412b58d4c5d100f24ad11c1ab9383b
Jahwffrey <johnthomason@umail.ucsb.edu>
2014-09-10 16:53:08 -0700

    Trying to get prerendering the page with resque to work

--- app/controllers/task_responses_controller.rb
+++ app/controllers/task_responses_controller.rb
@@ -23,95 +23,7 @@ class TaskResponsesController < ApplicationController
 
   def index
     if params['question'].present?
-      @question = AssessmentQuestion.find(params['question'])
-      @task = @question.assessment_task
-      @other_questions = @task.assessment_questions.where(assessment_question: nil)
-      @activity = @task.activity_page
-      @questions = (AssessmentQuestion.where(assessment_question: @question) << @question).rotate(-1) #bring the first variant back to front
-      @responses = AssessmentQuestionResponse.includes(:task_response).where(assessment_question: @questions)
 
-      @charts = []
-      case @question.question_type
-        when "singleAnswer"
-          @questions.each { |q|
-            response_hash = {}
-
-            #Gather response data
-            @responses.where(assessment_question: q).each { |r|
-              if JSON.parse(r.selected) == []
-                if response_hash["No Answer".to_sym].nil?
-                  response_hash.merge!("No Answer".to_sym => 1)
-                else
-                  response_hash["No Answer".to_sym]+=1
-                end
-              else
-                selected = JSON.parse(r.selected)[0]+1
-                if response_hash["Answer #{selected}".to_sym].nil?
-                  response_hash.merge!("Answer #{selected}".to_sym => 1)
-                else
-                  response_hash["Answer #{selected}".to_sym]+=1
-                end
-              end
-            }
-
-            #Gather correct answer
-            correct_answer = ""
-            JSON.parse(q.answers).each_with_index { |a, i|
-              if a['correct']==true
-                correct_answer=(i+1).to_s
-              end
-            }
-
-            @charts.push(
-                {:question_title => q.title,
-                 :chart => response_hash.sort,
-                 :correct_answer => correct_answer
-                }
-            )
-          }
-        when "multipleAnswers"
-          @questions.each { |q|
-            response_hash = {}
-
-            #Gather response data
-            @responses.where(assessment_question: q).each { |r|
-              selected = ""
-              JSON.parse(r.selected).sort.each { |x|
-                selected+=(x+1).to_s+", "
-              }
-              if selected == ""
-                if response_hash["No Answer".to_sym].nil?
-                  response_hash.merge!("No Answer".to_sym => 1)
-                else
-                  response_hash["No Answer".to_sym]+=1
-                end
-              else
-                selected = selected.chomp(', ')
-                if response_hash["Answers: #{selected}".to_sym].nil?
-                  response_hash.merge!("Answers: #{selected}".to_sym => 1)
-                else
-                  response_hash["Answers: #{selected}".to_sym]+=1
-                end
-              end
-            }
-
-            #Gather correct answer
-            correct_answer = ""
-            JSON.parse(q.answers).each_with_index { |a, i|
-              if a['correct']==true
-                correct_answer+=(i+1).to_s+", "
-              end
-            }
-            correct_answer = correct_answer.chomp(', ')
-
-            @charts.push(
-                {:question_title => q.title,
-                 :chart => response_hash.sort,
-                 :correct_answer => correct_answer
-                }
-            )
-          }
-      end
     else
       @curriculum_pages = CurriculumPage.accessible_by(@current_ability).order('title')
       @pages_map = @curriculum_pages.map { |cp|
@@ -157,9 +69,10 @@ class TaskResponsesController < ApplicationController
           correct_answer = ""
           JSON.parse(q.answers).each_with_index { |a, i|
             if a['correct']==true
-              correct_answer+=(i+1).to_s+" "
+              correct_answer+=(i+1).to_s+", "
             end
           }
+          correct_answer = correct_answer.chomp(', ')
         end
         wb.add_worksheet(:name => q.title) do |sheet|
           sheet.add_row ["School", "Class", "Student Number", (type=="freeResponse" ? "Response" : "Correct Response: #{correct_answer}")]
---
+++ app/workers/bundle_assessment_task_response_data.rb
@@ -0,0 +1,110 @@
+class BundleAssessmentTaskResponseData
+  @queue = :task_response_data
+
+  def self.perform(question_id)
+    control = ActionController::Base.new
+    @question = AssessmentQuestion.find(question_id)
+    @task = @question.assessment_task
+    @other_questions = @task.assessment_questions.where(assessment_question: nil)
+    @activity = @task.activity_page
+    @questions = (AssessmentQuestion.where(assessment_question: @question) << @question).rotate(-1) #bring the first variant back to front
+    @responses = AssessmentQuestionResponse.includes(:task_response).where(assessment_question: @questions)
+
+    @charts = []
+    case @question.question_type
+      when "singleAnswer"
+        @questions.each { |q|
+          response_hash = {}
+
+          #Gather response data
+          @responses.where(assessment_question: q).each { |r|
+            if JSON.parse(r.selected) == []
+              if response_hash["No Answer".to_sym].nil?
+                response_hash.merge!("No Answer".to_sym => 1)
+              else
+                response_hash["No Answer".to_sym]+=1
+              end
+            else
+              selected = JSON.parse(r.selected)[0]+1
+              if response_hash["Answer #{selected}".to_sym].nil?
+                response_hash.merge!("Answer #{selected}".to_sym => 1)
+              else
+                response_hash["Answer #{selected}".to_sym]+=1
+              end
+            end
+          }
+
+          #Gather correct answer
+          correct_answer = ""
+          JSON.parse(q.answers).each_with_index { |a, i|
+            if a['correct']==true
+              correct_answer=(i+1).to_s
+            end
+          }
+
+          @charts.push(
+              {:question_title => q.title,
+               :chart => response_hash.sort,
+               :correct_answer => correct_answer
+              }
+          )
+        }
+      when "multipleAnswers"
+        @questions.each { |q|
+          response_hash = {}
+
+          #Gather response data
+          @responses.where(assessment_question: q).each { |r|
+            selected = ""
+            JSON.parse(r.selected).sort.each { |x|
+              selected+=(x+1).to_s+", "
+            }
+            if selected == ""
+              if response_hash["No Answer".to_sym].nil?
+                response_hash.merge!("No Answer".to_sym => 1)
+              else
+                response_hash["No Answer".to_sym]+=1
+              end
+            else
+              selected = selected.chomp(', ')
+              if response_hash["Answers: #{selected}".to_sym].nil?
+                response_hash.merge!("Answers: #{selected}".to_sym => 1)
+              else
+                response_hash["Answers: #{selected}".to_sym]+=1
+              end
+            end
+          }
+
+          #Gather correct answer
+          correct_answer = ""
+          JSON.parse(q.answers).each_with_index { |a, i|
+            if a['correct']==true
+              correct_answer+=(i+1).to_s+", "
+            end
+          }
+          correct_answer = correct_answer.chomp(', ')
+
+          @charts.push(
+              {:question_title => q.title,
+               :chart => response_hash.sort,
+               :correct_answer => correct_answer
+              }
+          )
+        }
+    end
+
+
+
+    #render and save the webpage and spreadsheet
+    file_title = "AssessmentQuestion_#{question_id}_page"
+    begin
+      file = File.open("tmp/#{file_title}", "w")
+      rendered_page = control.render_to_string 'task_responses/taskresponsedata',locals: {question: @question}
+      file.write(rendered_page)
+    rescue IOError => e
+      #something went wrong...
+    ensure
+      file.close unless file == nil
+    end
+  end
+end
