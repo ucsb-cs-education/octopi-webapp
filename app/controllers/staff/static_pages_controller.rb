92db4df488e70302f15339e4f8be534c8f8b3c41
coheedac <bonbontim@hotmail.com>
2015-04-24 16:11:40 -0700

    Update LaPlaya and add read-only mode to dataviewer
    Also remove datepicker for sorting

--- app/controllers/staff/static_pages_controller.rb
+++ app/controllers/staff/static_pages_controller.rb
@@ -2,10 +2,10 @@ require 'laplaya_module'
 
 class Staff::StaticPagesController < ApplicationController
   before_action :authenticate_staff!
-  before_action :setup_laplaya, only: [:laplaya, :laplaya_file]
-  before_action :set_developer_mode, only: [:laplaya, :laplaya_file]
+  before_action :setup_laplaya, only: [:laplaya, :laplaya_file, :readonlylaplaya_file]
+  before_action :set_developer_mode, only: [:laplaya, :laplaya_file, :readonlylaplaya_file]
   before_action :force_trailing_slash, only: [:laplaya]
-  before_action :force_no_trailing_slash, only: [:laplaya_file]
+  before_action :force_no_trailing_slash, only: [:laplaya_file,:readonlylaplaya_file]
   include LaplayaModule
 
   def laplaya
@@ -15,6 +15,12 @@ class Staff::StaticPagesController < ApplicationController
   def laplaya_file
     authorize! :show, LaplayaFile.select(:id,:type).find(params[:id])
     @laplaya_ide_params[:fileID] = params[:id]
+    staff_laplaya_helper
+  end
+  def readonlylaplaya_file
+    authorize! :show, LaplayaFile.select(:id,:type).find(params[:id])
+    @laplaya_ide_params[:fileID] = params[:id]
+    @laplaya_ide_params[:readOnly] = true;
     staff_laplaya_helper
   end
 
